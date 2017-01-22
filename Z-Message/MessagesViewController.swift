57d58978e2851ef1fc6d49231bc5db08faf1a0fa
skerney <stevekerney@outlook.com>
2017-01-22 03:53:43 -0500

    asd

---
+++ Z-Message/MessagesViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MessagesViewController.swift
+//  Z-Message
+//
+//  Created by Steve Kerney on 12/29/16.
+//  Copyright © 2016 Steve Kerney. All rights reserved.
+//
+
+import UIKit
+
+
+class MessagesViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func LogoutPressed(_ sender: Any)
+    {
+        dismiss(animated: true)
+        {
+            
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
