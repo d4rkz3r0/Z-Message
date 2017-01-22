8bbc7d448748a6375176955ded0419338f681470
skerney <stevekerney@outlook.com>
2017-01-22 10:27:14 -0500

    Messaging added along with server side deletion of images. HQ and SQ added for mobile data connections.

---
+++ Z-Message/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Z-Message
+//
+//  Created by Steve Kerney on 1/22/17.
+//  Copyright © 2017 Steve Kerney. All rights reserved.
+//
+
+import Foundation
---
+++ Z-Message/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Z-Message
+//
+//  Created by Steve Kerney on 1/22/17.
+//  Copyright © 2017 Steve Kerney. All rights reserved.
+//
+
+import Foundation
